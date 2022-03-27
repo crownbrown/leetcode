9582c46af8e564f2c083febe6985510abdebde5b
crownbrown <sbthatcher@gmail.com>
2022-03-27 07:43:42 -0700

    Updated Test Suite - Wrote larger test suite, all pass

--- 13 - Roman to Int/tests.py
+++ 13 - Roman to Int/tests.py
@@ -14,3 +14,24 @@ class TestCase(unittest.TestCase):
 
     def test1d(self):
         self.assertEqual(romanToInt('IV'), 4)
+
+    def test2a(self):
+        self.assertEqual(romanToInt('IX'), 9)
+
+    def test2b(self):
+        self.assertEqual(romanToInt('XL'), 40)
+
+    def test2c(self):
+        self.assertEqual(romanToInt('XC'), 90)
+
+    def test2d(self):
+        self.assertEqual(romanToInt('CD'), 400)
+
+    def test2e(self):
+        self.assertEqual(romanToInt('CM'), 900)
+
+    def test3a(self):
+        self.assertEqual(romanToInt('LVIII'), 58)
+
+    def test3b(self):
+        self.assertEqual(romanToInt('MCMXCIV'), 1994)
