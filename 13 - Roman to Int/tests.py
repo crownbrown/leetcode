788fe70a703b88b27543b9c0d131f6c7e4fc5ffa
crownbrown <sbthatcher@gmail.com>
2022-03-27 07:11:05 -0700

    Failed Test #1b - Returning 2

--- 13 - Roman to Int/tests.py
+++ 13 - Roman to Int/tests.py
@@ -5,3 +5,6 @@ import unittest
 class TestCase(unittest.TestCase):
     def test1a(self):
         self.assertEqual(romanToInt('I'), 1)
+
+    def test1b(self):
+        self.assertEqual(romanToInt('II'), 2)
